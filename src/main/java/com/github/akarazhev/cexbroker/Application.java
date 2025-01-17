826a4c794b93c102cf52643d70d1234c6cdf4823
Andrey Karazhev <karazhev@gmail.com>
2025-01-18 00:00:52 +0300

    Implemented stubs for the main application.

--- src/main/java/com/github/akarazhev/cexbroker/Application.java
+++ src/main/java/com/github/akarazhev/cexbroker/Application.java
@@ -1,40 +1,45 @@
 package com.github.akarazhev.cexbroker;
 
 import com.github.akarazhev.cexbroker.bybit.config.Config;
-import com.github.akarazhev.cexbroker.function.Filters;
-import com.github.akarazhev.cexbroker.function.Mappers;
-import com.github.akarazhev.cexbroker.pipline.Observables;
+import com.github.akarazhev.cexbroker.stream.*;
 import io.reactivex.rxjava3.disposables.Disposable;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.util.Map;
 
 public class Application {
     private static final Logger LOGGER = LoggerFactory.getLogger(Application.class);
 
     public static void main(final String[] args) {
         final long t = System.currentTimeMillis();
-        final Config config = Config.getConfig();
-        LOGGER.info(config.toString());
+        LOGGER.info("Starting CEX Broker...");
+        LOGGER.info(Config.print());
 //        KafkaProducer<String, String> producer = new KafkaProducer<>(config.getKafkaProperties());
 //        MessageHandler messageHandler = new KafkaMessageHandler(producer, config.getKafkaTopic());
-        final Disposable bybitDisposable = Observables.ofBybit(config)
-                .map(Mappers.ofBybit())
-                .filter(Filters.ofBybit())
-                .subscribe(
-                message -> {
-                    LOGGER.info("Received: {}", message);
-//                    messageHandler.handleMessage(message);
-                },
-                throwable -> LOGGER.error("WebSocket error", throwable),
-                () -> {
-                    LOGGER.info("WebSocket closed");
-//                    producer.close();
-                }
-        );
+//        DataHandler handler = object -> LOGGER.info("Received: {}", object);
+        DataHandler handler = new DataHandler() {
+            @Override
+            public void handle(Map<String, Object> object) {
+                LOGGER.info("Received: {}", object);
+            }
+
+            @Override
+            public void close() {
+                LOGGER.info("Closing producer...");
+            }
+        };
+        final Mapper bybitMapper = Mappers.ofBybit();
+        final Filter bybitFilter = Filters.ofBybit();
+        final Subscriber bybitSubscriber = Subscribers.ofBybit(handler);
+        final Disposable bybitDisposable = Observables.ofBybit()
+                .map(bybitMapper.map())
+                .filter(bybitFilter.filter())
+                .subscribe(bybitSubscriber.onNext(), bybitSubscriber.onError(), bybitSubscriber.onComplete());
         Runtime.getRuntime().addShutdownHook(new Thread(() -> {
             LOGGER.info("Shutting down CEX Broker...");
             bybitDisposable.dispose();
         }));
-        LOGGER.info("CEX Broker started after {} ms", System.currentTimeMillis() - t);
+        LOGGER.info("CEX Broker has been started in {} ms", System.currentTimeMillis() - t);
     }
 }
