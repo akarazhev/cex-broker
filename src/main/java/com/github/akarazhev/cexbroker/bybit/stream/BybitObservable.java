176cb54b0e73cc9505e5261069c573ad70d917d9
Andrey Karazhev <karazhev@gmail.com>
2025-01-18 10:42:42 +0300

    Improved reconnection management.

--- src/main/java/com/github/akarazhev/cexbroker/bybit/stream/BybitObservable.java
+++ src/main/java/com/github/akarazhev/cexbroker/bybit/stream/BybitObservable.java
@@ -13,8 +13,11 @@ import org.java_websocket.client.WebSocketClient;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.time.Duration;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 public final class BybitObservable implements ObservableOnSubscribe<String> {
     private final static Logger LOGGER = LoggerFactory.getLogger(BybitObservable.class);
@@ -22,6 +25,7 @@ public final class BybitObservable implements ObservableOnSubscribe<String> {
     private final static long MAX_RECONNECT_DELAY = 30000; // Max delay of 30 seconds
     private final static long PING_INTERVAL = 20000; // 20 seconds
 
+    private final Lock reconnectLock = new ReentrantLock();
     private final AtomicInteger reconnectAttempts = new AtomicInteger(0);
 
     private WebSocketClient client;
@@ -63,28 +67,35 @@ public final class BybitObservable implements ObservableOnSubscribe<String> {
             }
 
             private void reconnect(String reason) {
-                while (reconnectAttempts.get() < MAX_RECONNECT_ATTEMPTS) {
-                    final int attempts = reconnectAttempts.getAndIncrement();
-                    final long delay = Math.min(1000 * (long) Math.pow(2, attempts), MAX_RECONNECT_DELAY);
-                    LOGGER.warn("{}. Attempting to reconnect in {} ms... (Attempt {})", reason, delay, attempts + 1);
+                if (reconnectLock.tryLock()) {
                     try {
-                        client = Clients.newWsClient(Config.getWebSocketUri(), this);
-                        if (client.connectBlocking(delay, TimeUnit.MILLISECONDS)) {
-                            LOGGER.warn("Reconnected after {} ms", delay);
-                            emitter.setCancellable(client::close);
-                            return; // Successfully reconnected, exit the method
-                        } else {
-                            reason = "Failed to reconnect";
+                        while (reconnectAttempts.get() < MAX_RECONNECT_ATTEMPTS) {
+                            final int attempts = reconnectAttempts.getAndIncrement();
+                            final long delay = Math.min(1000 * (long) Math.pow(2, attempts), MAX_RECONNECT_DELAY);
+                            LOGGER.warn("{}. Attempting to reconnect in {} ms... (Attempt {})", reason, delay, attempts + 1);
+                            try {
+                                 client = Clients.newWsClient(Config.getWebSocketUri(), this);
+                                if (client.connectBlocking()) {
+                                    LOGGER.warn("Reconnected after {} ms", delay);
+                                    emitter.setCancellable(client::close);
+                                    return; // Successfully reconnected, exit the method
+                                } else {
+                                    reason = "Failed to reconnect";
+                                    Thread.sleep(Duration.ofMillis(delay));
+                                }
+                            } catch (InterruptedException e) {
+                                LOGGER.error("Error reconnecting to the server: {}", e.getMessage());
+                                Thread.currentThread().interrupt(); // Restore the interrupt status
+                                return; // Exit the reconnection loop if interrupted
+                            }
                         }
-                    } catch (InterruptedException e) {
-                        LOGGER.error("Error reconnecting to the server: {}", e.getMessage());
-                        Thread.currentThread().interrupt(); // Restore the interrupt status
-                        return; // Exit the reconnection loop if interrupted
+                        // If we've exhausted all attempts
+                        LOGGER.error("Max reconnection attempts reached. Closing observable.");
+                        emitter.onComplete();
+                    } finally {
+                        reconnectLock.unlock();
                     }
                 }
-                // If we've exhausted all attempts
-                LOGGER.error("Max reconnection attempts reached. Closing observable.");
-                emitter.onComplete();
             }
 
             private void startPing() {
