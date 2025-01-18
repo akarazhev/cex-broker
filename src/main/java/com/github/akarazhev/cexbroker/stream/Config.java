cd09a4a6da4ca21b294e5cbb2cc34a0337df7cf5
Andrey Karazhev <karazhev@gmail.com>
2025-01-18 23:58:19 +0300

    Simplified kafka properties.

--- src/main/java/com/github/akarazhev/cexbroker/stream/Config.java
+++ src/main/java/com/github/akarazhev/cexbroker/stream/Config.java
@@ -3,19 +3,12 @@ package com.github.akarazhev.cexbroker.stream;
 import java.util.Properties;
 
 public final class Config {
-    private final static String KAFKA_BOOTSTRAP_SERVERS;
-    private final static String KAFKA_TOPIC;
     private final static Properties KAFKA_PROPERTIES;
 
     static {
-        String value = System.getenv("KAFKA_TOPIC");
-        KAFKA_TOPIC = value != null ? value : "CEX_BROKER";
-
-        value = System.getenv("KAFKA_BOOTSTRAP_SERVERS");
-        KAFKA_BOOTSTRAP_SERVERS = value != null ? value : "localhost:9092";
-
         KAFKA_PROPERTIES = new Properties();
-        KAFKA_PROPERTIES.put("bootstrap.servers", KAFKA_BOOTSTRAP_SERVERS);
+        final String value = System.getenv("KAFKA_BOOTSTRAP_SERVERS");
+        KAFKA_PROPERTIES.put("bootstrap.servers", value != null ? value : "localhost:9092");
         KAFKA_PROPERTIES.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
         KAFKA_PROPERTIES.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
     }
@@ -24,22 +17,13 @@ public final class Config {
         throw new UnsupportedOperationException();
     }
 
-    public static String getKafkaTopic() {
-        return KAFKA_TOPIC;
-    }
-
-    public static String getKafkaBootstrapServers() {
-        return KAFKA_BOOTSTRAP_SERVERS;
-    }
-
     public static Properties getKafkaProperties() {
         return KAFKA_PROPERTIES;
     }
 
     public static String print() {
         return "Kafka Config {" +
-                "kafkaBootstrapServers='" + KAFKA_BOOTSTRAP_SERVERS + '\'' +
-                ", kafkaTopic='" + KAFKA_TOPIC + '\'' +
+                "kafkaProperties='" + getKafkaProperties() + '\'' +
                 '}';
     }
 }
