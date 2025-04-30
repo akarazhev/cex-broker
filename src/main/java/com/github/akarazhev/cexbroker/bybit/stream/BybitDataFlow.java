453d0f85d1ce4bb501034a8386cfb2747e658c03
Andrey Karazhev <andrey.karazhev@specific-group.com>
2025-04-30 09:49:33 +0300

    Set time-out to 0, manage bytes.

--- src/main/java/com/github/akarazhev/cexbroker/bybit/stream/BybitDataFlow.java
+++ src/main/java/com/github/akarazhev/cexbroker/bybit/stream/BybitDataFlow.java
@@ -11,6 +11,7 @@ import okhttp3.Request;
 import okhttp3.Response;
 import okhttp3.WebSocket;
 import okhttp3.WebSocketListener;
+import okio.ByteString;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -26,7 +27,9 @@ public final class BybitDataFlow implements FlowableOnSubscribe<String> {
     private WebSocket webSocket;
 
     private BybitDataFlow() {
-        this.client = new OkHttpClient();
+        this.client = new OkHttpClient.Builder()
+                .readTimeout(0, TimeUnit.MILLISECONDS)
+                .build();
     }
 
     public static BybitDataFlow create() {
@@ -67,6 +70,12 @@ public final class BybitDataFlow implements FlowableOnSubscribe<String> {
                     LOGGER.info("Received message: {}", text);
                     emitter.onNext(text);
                 }
+            }
+
+            @Override
+            public void onMessage(final WebSocket ws, final ByteString bytes) {
+                LOGGER.debug("Received bytes: {}", bytes.hex());
+                emitter.onNext(bytes.utf8());
             }
 
             @Override
