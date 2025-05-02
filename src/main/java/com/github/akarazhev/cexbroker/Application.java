3ddc17aeef0425d1361ffa4f63eeb6a7d2c425bf
Andrey Karazhev <andrey.karazhev@specific-group.com>
2025-05-02 17:24:14 +0300

    Extended configuration with orders and new endpoints.

--- src/main/java/com/github/akarazhev/cexbroker/Application.java
+++ src/main/java/com/github/akarazhev/cexbroker/Application.java
@@ -1,6 +1,5 @@
 package com.github.akarazhev.cexbroker;
 
-import com.github.akarazhev.cexbroker.bybit.BybitConfig;
 import com.github.akarazhev.cexbroker.bybit.stream.BybitFilter;
 import com.github.akarazhev.cexbroker.bybit.stream.BybitMapper;
 import com.github.akarazhev.cexbroker.bybit.stream.BybitSubscriber;
@@ -10,6 +9,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.Map;
+
+import static com.github.akarazhev.cexbroker.bybit.BybitConfig.getPublicSubscribeTopics;
+import static com.github.akarazhev.cexbroker.bybit.BybitConfig.getPublicTestnetSpot;
 
 public final class Application {
     private static final Logger LOGGER = LoggerFactory.getLogger(Application.class);
@@ -30,15 +32,13 @@ public final class Application {
             }
         };
         final var bybitSubscriber = BybitSubscriber.create(consoleHandler);
-        final var url = BybitConfig.getPublicTestnetSpot();
-        final var topics = BybitConfig.getPublicSubscribeTopics();
-        final var bybitDisposable = DataFlows.ofBybit(url, topics)
+        final var publicTestnetSpot = DataFlows.ofBybit(getPublicTestnetSpot(), getPublicSubscribeTopics())
                 .map(BybitMapper.ofMap())
                 .filter(BybitFilter.ofFilter())
                 .subscribe(bybitSubscriber.onNext(), bybitSubscriber.onError(), bybitSubscriber.onComplete());
         Runtime.getRuntime().addShutdownHook(new Thread(() -> {
             LOGGER.info("Shutting down CEX Broker...");
-            bybitDisposable.dispose();
+            publicTestnetSpot.dispose();
             consoleHandler.close();
         }));
         LOGGER.info("CEX Broker has been started in {} ms", System.currentTimeMillis() - t);
--- src/main/java/com/github/akarazhev/cexbroker/bybit/BybitConfig.java
+++ src/main/java/com/github/akarazhev/cexbroker/bybit/BybitConfig.java
@@ -5,9 +5,17 @@ import com.github.akarazhev.cexbroker.config.AppConfig;
 import java.util.Arrays;
 
 public final class BybitConfig {
+    // Endpoints are defined in application.properties
     private static final String PUBLIC_TESTNET_SPOT = AppConfig.getAsString("bybit.public.testnet.spot");
+    private static final String PUBLIC_TESTNET_LINEAR = AppConfig.getAsString("bybit.public.testnet.linear");
+    private static final String PUBLIC_TESTNET_INVERSE = AppConfig.getAsString("bybit.public.testnet.inverse");
+    private static final String PUBLIC_TESTNET_OPTION = AppConfig.getAsString("bybit.public.testnet.option");
+    private static final String PUBLIC_TESTNET_SPREAD = AppConfig.getAsString("bybit.public.testnet.spread");
+    // Topics are defined in application.properties
     private static final String[] PUBLIC_SUBSCRIBE_TOPICS = AppConfig.getAsString("bybit.public.subscribe.topics").split(",");
+    // Ping interval is defined in application.properties
     private static final long PING_INTERVAL = AppConfig.getAsLong("bybit.ping.interval");
+    // Reconnect interval are defined in application.properties
     private static final long RECONNECT_INTERVAL = AppConfig.getAsLong("bybit.reconnect.interval");
 
     private BybitConfig() {
@@ -16,6 +24,22 @@ public final class BybitConfig {
 
     public static String getPublicTestnetSpot() {
         return PUBLIC_TESTNET_SPOT;
+    }
+
+    public static String getPublicTestnetLinear() {
+        return PUBLIC_TESTNET_LINEAR;
+    }
+
+    public static String getPublicTestnetInverse() {
+        return PUBLIC_TESTNET_INVERSE;
+    }
+
+    public static String getPublicTestnetOption() {
+        return PUBLIC_TESTNET_OPTION;
+    }
+
+    public static String getPublicTestnetSpread() {
+        return PUBLIC_TESTNET_SPREAD;
     }
 
     public static String[] getPublicSubscribeTopics() {
@@ -33,6 +57,10 @@ public final class BybitConfig {
     public static String print() {
         return "Bybit Config {" +
                 "publicTestnetSpot=" + getPublicTestnetSpot() +
+                ", publicTestnetLinear=" + getPublicTestnetLinear() +
+                ", publicTestnetInverse=" + getPublicTestnetInverse() +
+                ", publicTestnetOption=" + getPublicTestnetOption() +
+                ", publicTestnetSpread=" + getPublicTestnetSpread() +
                 ", publicSubscribeTopics=" + Arrays.toString(getPublicSubscribeTopics()) +
                 ", pingInterval=" + getPingInterval() +
                 ", reconnectInterval=" + getReconnectInterval() +
